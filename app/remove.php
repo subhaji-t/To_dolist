612d4ab1133f761807b27b15f9d9c44f3666724d
subhaji-t <122025579+subhaji-t@users.noreply.github.com>
2023-07-10 01:40:04 +0530

    Update remove.php

--- app/remove.php
+++ app/remove.php
@@ -1,1 +1,37 @@
+<?php
+if(isset($_POST['id'])){
+    $sName = "localhost";
+    $uName = "root";
+    $pass = "";
+    $db_name = "to_do_list";
 
+    $conn = new mysqli($sName, $uName, $pass, $db_name);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $id = $_POST['id'];
+
+    if (empty($id)) {
+        echo 0;
+    } else {
+        $stmt = $conn->prepare("DELETE FROM todos WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $res = $stmt->execute();
+
+        if ($res) {
+            echo 1;
+        } else {
+            echo 0;
+        }
+
+        $stmt->close();
+        $conn->close();
+
+        exit();
+    }
+} else {
+    header("Location: ../index.php?mess=error");
+}
+?>
