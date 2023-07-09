8a8bc6673fa29c7417d500c9bdf1f4f30ddd15b2
subhaji-t <122025579+subhaji-t@users.noreply.github.com>
2023-07-10 01:14:00 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,20 +1,15 @@
 <?php
 
-$sName = "localhost";   // The server name (usually 'localhost')
-$uName = "root";        // The username for the database
-$pass = "";             // The password for the database
-$db_name = "to_do_list"; // The name of the database you want to connect to
+$sName = "localhost";
+$uName = "root";
+$pass = "";
+$db_name = "to_do_list"; 
 
-// Create a MySQLi object and establish the connection
 $conn = new mysqli($sName, $uName, $pass, $db_name);
-
-// Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 echo "Connected successfully!";
-
-// Close the connection when done
 $conn->close();
 ?>
