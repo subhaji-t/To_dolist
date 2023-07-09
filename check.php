afdeed4a2975380cb787de1624c090d73e83b16b
subhaji-t <122025579+subhaji-t@users.noreply.github.com>
2023-07-10 01:12:46 +0530

    Update check.php

--- check.php
+++ check.php
@@ -1,14 +1,11 @@
 <?php
 if(isset($_POST['id'])){
-    $sName = "localhost";     // The server name (usually 'localhost')
-    $uName = "root";          // The username for the database
-    $pass = "";               // The password for the database
-    $db_name = "to_do_list";  // The name of the database you want to connect to
+    $sName = "localhost";     
+    $uName = "root";          
+    $pass = "";               
+    $db_name = "to_do_list";  
 
-    // Create a MySQLi object and establish the connection
     $conn = new mysqli($sName, $uName, $pass, $db_name);
-
-    // Check if the connection was successful
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
@@ -34,8 +31,6 @@ if(isset($_POST['id'])){
         }else {
             echo "error";
         }
-
-        // Close the database connection
         $conn->close();
 
         exit();
